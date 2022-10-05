d9e38fdb38f0b252e0c70aede117dd41f246dac8
Victor Nathanael <victornathanael15@gmail.com>
2022-10-04 22:23:28 -0300

    fix: remove credentials from github

--- repositories/db.js
+++ repositories/db.js
@@ -5,8 +5,7 @@ async function connect() {
         return global.connection.connect();
     }
     const pool = new pg.Pool({
-        connectionString:
-            'postgres://wtujzvqt:ioGsi6xtQudyl7_OIFK3ovkrJuq2P3M3@babar.db.elephantsql.com/wtujzvqt',
+        connectionString: process.env.DB_CONN_STRING,
     });
     global.connection = pool;
 
