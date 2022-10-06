04d8e127d84df094a288fdbdd754698eb011e1e2
Victor Nathanael <victornathanael15@gmail.com>
2022-10-06 20:06:46 -0300

    feat: add crud on clients

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -14,8 +14,60 @@ async function createClient(req, res, next) {
                 'Name, CPF, Phone, Email e Address são obrigatórios.'
             );
         }
-        res.send(await ClientService.createClient(client));
+        client = await ClientService.createClient(client);
+        res.send(client);
         logger.info(`POST /client - ${JSON.stringify(client)}`);
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function getClients(req, res, next) {
+    try {
+        res.send(await ClientService.getClients());
+        logger.info('GET /client');
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function getClient(req, res, next) {
+    try {
+        res.send(await ClientService.getClient(req.params.id));
+        logger.info('GET /client');
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function deleteClient(req, res, next) {
+    try {
+        await ClientService.deleteClient(req.params.id);
+        res.end();
+        logger.info('DELETE /client');
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function updateClient(req, res, next) {
+    try {
+        let client = req.body;
+        if (
+            !client.client_id ||
+            !client.name ||
+            !client.cpf ||
+            !client.phone ||
+            !client.email ||
+            !client.address
+        ) {
+            throw new Error(
+                'Client ID, Name, CPF, Phone, Email e Address são obrigatórios.'
+            );
+        }
+        client = await ClientService.updateClient(client);
+        res.send(client);
+        logger.info(`PUT /client - ${JSON.stringify(client)}`);
     } catch (err) {
         next(err);
     }
@@ -23,4 +75,8 @@ async function createClient(req, res, next) {
 
 export default {
     createClient,
+    getClients,
+    getClient,
+    deleteClient,
+    updateClient,
 };
